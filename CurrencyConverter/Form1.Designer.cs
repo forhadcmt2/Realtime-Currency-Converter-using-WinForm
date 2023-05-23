3278e36e4263d71c750cb773973762c04a070c9a
Forhad Hossain <forhad@ghost.com.bd>
2023-05-23 20:11:14 +0600

    Project initially completed

--- CurrencyConverter/Form1.Designer.cs
+++ CurrencyConverter/Form1.Designer.cs
@@ -28,31 +28,44 @@
         /// </summary>
         private void InitializeComponent()
         {
-            button1 = new Button();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            convertBtn = new Button();
             label2 = new Label();
             label1 = new Label();
-            comboBox1 = new ComboBox();
-            comboBox2 = new ComboBox();
+            fromCurrencyCombo = new ComboBox();
+            toCurrencyCombo = new ComboBox();
+            panel1 = new Panel();
+            startBtn = new Button();
+            label3 = new Label();
+            exchangeTxt = new Label();
+            label4 = new Label();
+            amountTextBox = new TextBox();
+            refreshBtn = new Button();
+            panel1.SuspendLayout();
             SuspendLayout();
             // 
-            // button1
+            // convertBtn
             // 
-            button1.Location = new Point(491, 362);
-            button1.Margin = new Padding(4, 5, 4, 5);
-            button1.Name = "button1";
-            button1.Size = new Size(211, 58);
-            button1.TabIndex = 0;
-            button1.Text = "Click Me";
-            button1.UseVisualStyleBackColor = true;
-            button1.Click += button1_Click;
+            convertBtn.Font = new Font("Segoe UI", 14.25F, FontStyle.Bold, GraphicsUnit.Point);
+            convertBtn.ForeColor = SystemColors.ButtonFace;
+            convertBtn.Image = Properties.Resources.Rectangle_5;
+            convertBtn.Location = new Point(449, 493);
+            convertBtn.Margin = new Padding(4, 5, 4, 5);
+            convertBtn.Name = "convertBtn";
+            convertBtn.Size = new Size(333, 60);
+            convertBtn.TabIndex = 0;
+            convertBtn.Text = "CONVERT";
+            convertBtn.UseVisualStyleBackColor = true;
+            convertBtn.Click += ConvertBtn_Click;
             // 
             // label2
             // 
             label2.AutoSize = true;
-            label2.Location = new Point(333, 257);
+            label2.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            label2.Location = new Point(449, 378);
             label2.Margin = new Padding(4, 0, 4, 0);
             label2.Name = "label2";
-            label2.Size = new Size(30, 25);
+            label2.Size = new Size(39, 32);
             label2.TabIndex = 4;
             label2.Text = "To";
             label2.TextAlign = ContentAlignment.MiddleRight;
@@ -60,53 +73,144 @@
             // label1
             // 
             label1.AutoSize = true;
-            label1.Location = new Point(333, 188);
+            label1.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            label1.Location = new Point(449, 285);
             label1.Margin = new Padding(4, 0, 4, 0);
             label1.Name = "label1";
-            label1.Size = new Size(54, 25);
+            label1.Size = new Size(69, 32);
             label1.TabIndex = 2;
             label1.Text = "From";
             label1.TextAlign = ContentAlignment.MiddleRight;
             // 
-            // comboBox1
+            // fromCurrencyCombo
             // 
-            comboBox1.FormattingEnabled = true;
-            comboBox1.Location = new Point(429, 188);
-            comboBox1.Name = "comboBox1";
-            comboBox1.Size = new Size(331, 33);
-            comboBox1.TabIndex = 5;
+            fromCurrencyCombo.FormattingEnabled = true;
+            fromCurrencyCombo.Location = new Point(449, 323);
+            fromCurrencyCombo.Name = "fromCurrencyCombo";
+            fromCurrencyCombo.Size = new Size(331, 33);
+            fromCurrencyCombo.TabIndex = 5;
             // 
-            // comboBox2
+            // toCurrencyCombo
             // 
-            comboBox2.FormattingEnabled = true;
-            comboBox2.Location = new Point(429, 257);
-            comboBox2.Name = "comboBox2";
-            comboBox2.Size = new Size(331, 33);
-            comboBox2.TabIndex = 6;
+            toCurrencyCombo.FormattingEnabled = true;
+            toCurrencyCombo.Location = new Point(449, 423);
+            toCurrencyCombo.Name = "toCurrencyCombo";
+            toCurrencyCombo.Size = new Size(331, 33);
+            toCurrencyCombo.TabIndex = 6;
+            // 
+            // panel1
+            // 
+            panel1.Controls.Add(startBtn);
+            panel1.Location = new Point(0, 2);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(1141, 702);
+            panel1.TabIndex = 7;
+            // 
+            // startBtn
+            // 
+            startBtn.Location = new Point(446, 328);
+            startBtn.Margin = new Padding(4, 5, 4, 5);
+            startBtn.Name = "startBtn";
+            startBtn.Size = new Size(277, 100);
+            startBtn.TabIndex = 0;
+            startBtn.Text = "Start";
+            startBtn.UseVisualStyleBackColor = true;
+            startBtn.Click += StartBtn_ClickAsync;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Segoe UI", 9.75F, FontStyle.Regular, GraphicsUnit.Point);
+            label3.ForeColor = SystemColors.ActiveCaptionText;
+            label3.Location = new Point(553, 50);
+            label3.Margin = new Padding(4, 0, 4, 0);
+            label3.Name = "label3";
+            label3.Size = new Size(138, 28);
+            label3.TabIndex = 8;
+            label3.Text = "Exchange Rate";
+            // 
+            // exchangeTxt
+            // 
+            exchangeTxt.AutoSize = true;
+            exchangeTxt.Font = new Font("Segoe UI", 24F, FontStyle.Bold, GraphicsUnit.Point);
+            exchangeTxt.Location = new Point(553, 95);
+            exchangeTxt.Margin = new Padding(4, 0, 4, 0);
+            exchangeTxt.Name = "exchangeTxt";
+            exchangeTxt.Size = new Size(140, 65);
+            exchangeTxt.TabIndex = 9;
+            exchangeTxt.Text = "$100";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            label4.Location = new Point(449, 193);
+            label4.Margin = new Padding(4, 0, 4, 0);
+            label4.Name = "label4";
+            label4.Size = new Size(218, 32);
+            label4.TabIndex = 10;
+            label4.Text = "Exchange Amounts";
+            label4.TextAlign = ContentAlignment.MiddleRight;
+            // 
+            // amountTextBox
+            // 
+            amountTextBox.Location = new Point(449, 233);
+            amountTextBox.Margin = new Padding(4, 5, 4, 5);
+            amountTextBox.Name = "amountTextBox";
+            amountTextBox.Size = new Size(331, 31);
+            amountTextBox.TabIndex = 11;
+            // 
+            // refreshBtn
+            // 
+            refreshBtn.Font = new Font("Segoe UI", 14.25F, FontStyle.Bold, GraphicsUnit.Point);
+            refreshBtn.ForeColor = SystemColors.ActiveCaptionText;
+            refreshBtn.Image = (Image)resources.GetObject("refreshBtn.Image");
+            refreshBtn.Location = new Point(449, 578);
+            refreshBtn.Margin = new Padding(4, 5, 4, 5);
+            refreshBtn.Name = "refreshBtn";
+            refreshBtn.Size = new Size(333, 60);
+            refreshBtn.TabIndex = 12;
+            refreshBtn.Text = "Refresh";
+            refreshBtn.UseVisualStyleBackColor = true;
+            refreshBtn.Click += refreshBtn_Click;
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(10F, 25F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(1143, 750);
-            Controls.Add(comboBox2);
-            Controls.Add(comboBox1);
+            ClientSize = new Size(1143, 702);
+            Controls.Add(panel1);
+            Controls.Add(refreshBtn);
+            Controls.Add(amountTextBox);
+            Controls.Add(label4);
+            Controls.Add(exchangeTxt);
+            Controls.Add(label3);
+            Controls.Add(toCurrencyCombo);
+            Controls.Add(fromCurrencyCombo);
             Controls.Add(label2);
             Controls.Add(label1);
-            Controls.Add(button1);
+            Controls.Add(convertBtn);
             Margin = new Padding(4, 5, 4, 5);
             Name = "Form1";
             Text = "Form1";
+            panel1.ResumeLayout(false);
             ResumeLayout(false);
             PerformLayout();
         }
 
         #endregion
 
-        private Button button1;
+        private Button convertBtn;
         private Label label2;
         private Label label1;
-        private ComboBox comboBox1;
-        private ComboBox comboBox2;
+        private ComboBox fromCurrencyCombo;
+        private ComboBox toCurrencyCombo;
+        private Panel panel1;
+        private Button startBtn;
+        private Label label3;
+        private Label exchangeTxt;
+        private Label label4;
+        private TextBox amountTextBox;
+        private Button refreshBtn;
     }
 }
--- CurrencyConverter/Form1.cs
+++ CurrencyConverter/Form1.cs
@@ -1,84 +1,106 @@
 using System.Net.Http.Headers;
 using System.Text.Json.Nodes;
 using System.Text.Json;
-using System.Text.Json.Serialization;
-using System.Windows.Forms;
-using System.Text;
+
 
 namespace CurrencyConverter
 {
     public partial class Form1 : Form
     {
+        private string baseUri = "https://cdn.jsdelivr.net/";
+        private string currencyListUri = "gh/fawazahmed0/currency-api@1/latest/currencies.json";
+
+        private string usdBasedCurrencyValueUri = "gh/fawazahmed0/currency-api@1/latest/currencies/usd.json";
+
+        private List<string>? currencyNames = new List<string>();
+        private List<double>? currencyValues = new List<double>();
+
         public Form1()
         {
             InitializeComponent();
         }
 
-        private void button1_Click(object sender, EventArgs e)
+        #region Button Click Method
+        private void ConvertBtn_Click(object sender, EventArgs e)
         {
-            EnableDisableButton(button1, "Working....", false);
-
-            HttpClient client = new HttpClient
+            if (string.IsNullOrEmpty(amountTextBox.Text))
             {
-                BaseAddress = new Uri("https://cdn.jsdelivr.net/")
-            };
-
-            client.Timeout = TimeSpan.FromSeconds(10);
-
-            // Add an Accept header for JSON format.
-            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
-
-            //Get Response from the api
-            HttpResponseMessage response = client.GetAsync("gh/fawazahmed0/currency-api@1/latest/currencies.json").Result;
-
-            if (response != null && response.IsSuccessStatusCode)
-            {
-                JsonNode? node = JsonNode.Parse(response.Content.ReadAsStringAsync().Result);
-
-                Dictionary<string, string>? currencyDictionary = node.Deserialize<Dictionary<string, string>>();
-
-                DialogResult dialogResult = DialogResult.None;
-
-                if (node == null)
-                {
-                    dialogResult = MessageBox.Show("Got Error while processing api data!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
-                }
-
-                if (currencyDictionary == null)
-                {
-                    dialogResult = MessageBox.Show("API returned invalid data, please check", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
-                }
-
-                if (dialogResult == DialogResult.OK)
-                {
-                    EnableDisableButton(button1, "Click Me", true);
-                    return;
-                }
-
-                string allKeys = string.Empty;
-                string tempValue = string.Empty;
-                foreach (string key in currencyDictionary.Keys)
-                {
-                    tempValue = key.ToUpperInvariant() + "- " + currencyDictionary[key] + "\n";
-                    allKeys += tempValue;
-                    comboBox1.Items.Add(tempValue);
-                    comboBox2.Items.Add(tempValue);
-                }
-
-                comboBox1.SelectedIndex = 50;
-                comboBox2.SelectedIndex = 4;
-
-                dialogResult = MessageBox.Show(allKeys, "Currency List", MessageBoxButtons.OK, MessageBoxIcon.Information);
-                if (dialogResult == DialogResult.OK)
-                    EnableDisableButton(button1, "Click Me", true);
-            }
-            else
-            {
-                DialogResult dialogResult = MessageBox.Show($"Got Error: {response.StatusCode}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
-                if (dialogResult == DialogResult.OK)
-                    EnableDisableButton(button1, "Click Me", true);
+                ShowDataParseError("You have not entered any number. Please input a valid number.");
+                return;
             }
 
+            if (currencyNames == null || currencyValues == null)
+            {
+                MessageBox.Show("List is null!");
+                return;
+            }
+
+            bool parseSuccess = float.TryParse(amountTextBox.Text.ToString(), out float exchangeAmount);
+
+            if (parseSuccess)
+            {
+                ConvertCurrency();
+                return;
+            }
+
+            ShowDataParseError("You have entered an invalid number. Please input a valid number.");
+        }
+
+        private async void StartBtn_ClickAsync(object sender, EventArgs e)
+        {
+            EnableDisableButton(startBtn, "Please wait....", false);
+
+            string currencyNameList = await SendHTTPRequest(currencyListUri, ResetStartPageToNormal);
+            string currencyValue = await SendHTTPRequest(usdBasedCurrencyValueUri, ResetStartPageToNormal);
+
+            ParseCurrencyList(currencyNameList);
+            ParseCurrencyValues(currencyValue);
+        }
+
+        private void refreshBtn_Click(object sender, EventArgs e)
+        {
+            EnableDisableStartPanel(true);
+            StartBtn_ClickAsync(sender, e);
+        }
+        #endregion Button Click Methods
+
+        #region Currency Conversion Logic
+        private void ConvertCurrency()
+        {
+            try
+            {
+                double amount = double.Parse(amountTextBox.Text);
+                double fromCurrencyInUSD = (1.0f / currencyValues[fromCurrencyCombo.SelectedIndex]) * amount;
+                double result = fromCurrencyInUSD * currencyValues[toCurrencyCombo.SelectedIndex];
+                exchangeTxt.Text = result.ToString("0.00");
+            }
+            catch (Exception ex)
+            {
+                ShowDataParseError("ConvertCurrency: " + ex.Message);
+            }
+        }
+        #endregion Currency Conversion Logic
+
+        #region UI Control Section
+        private void EnableDisableStartPanel(bool value)
+        {
+            panel1.Visible = value;
+            startBtn.Visible = value;
+            startBtn.Enabled = value;
+
+            if (value)
+                startBtn.Text = "Start";
+        }
+
+        private void EnableDisableInputAndButtons(bool enable)
+        {
+            fromCurrencyCombo.Enabled = enable;
+            toCurrencyCombo.Enabled = enable;
+
+            convertBtn.Enabled = enable;
+            refreshBtn.Enabled = enable;
+
+            exchangeTxt.Enabled = enable;
         }
 
         private static void EnableDisableButton(Button button, string btnText, bool shouldInteract)
@@ -86,5 +108,142 @@ namespace CurrencyConverter
             button.Enabled = shouldInteract;
             button.Text = btnText;
         }
+        #endregion UI Control Section
+
+        #region Error Handling
+        private void ResetStartPageToNormal(string errorMessage)
+        {
+            DialogResult dialogResult = MessageBox.Show(errorMessage, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            if (dialogResult == DialogResult.OK)
+                EnableDisableStartPanel(true);
+        }
+
+        private void ShowDataParseError(string errorMessage)
+        {
+            DialogResult dialogResult = MessageBox.Show(errorMessage, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+            if (dialogResult == DialogResult.OK)
+            {
+                EnableDisableInputAndButtons(true);
+            }
+        }
+        #endregion Error Handling
+
+        #region Data Parsing
+        private void ParseCurrencyList(string result)
+        {
+            if (string.IsNullOrEmpty(result))
+                return;
+
+            try
+            {
+                JsonNode? node = JsonNode.Parse(result);
+
+                Dictionary<string, string> currencyNameDic = node.Deserialize<Dictionary<string, string>>();
+
+                int usdIndex = 0;
+                int bdtIndex = 0;
+
+                currencyNames.Clear();
+
+                fromCurrencyCombo.Items.Clear();
+                toCurrencyCombo.Items.Clear();
+
+                foreach (string key in currencyNameDic.Keys)
+                {
+                    string tempValue = key.ToUpperInvariant() + " - " + currencyNameDic[key] + "\n";
+                    fromCurrencyCombo.Items.Add(tempValue);
+                    toCurrencyCombo.Items.Add(tempValue);
+
+                    currencyNames.Add(key);
+
+                    if (key.Equals("bdt"))
+                        bdtIndex = fromCurrencyCombo.Items.Count - 1;
+
+                    if (key.Equals("usd"))
+                        usdIndex = fromCurrencyCombo.Items.Count - 1;
+                }
+
+                fromCurrencyCombo.SelectedIndex = usdIndex;
+                toCurrencyCombo.SelectedIndex = bdtIndex;
+            }
+            catch (Exception ex)
+            {
+                ResetStartPageToNormal($"Got Error of {ex.Message} while downloading currency list from the api!");
+            }
+        }
+
+        private void ParseCurrencyValues(string result)
+        {
+            try
+            {
+                if (string.IsNullOrEmpty(result))
+                    return;
+
+                JsonNode? node = JsonNode.Parse(result);
+
+                var jObject = JsonObject.Parse(result);
+
+                Dictionary<string, double> currencyValueDic = node["usd"].Deserialize<Dictionary<string, double>>();
+                amountTextBox.Text = "1";
+
+                currencyValues.Clear();
+
+                currencyValues = currencyValueDic.Values.ToList();
+                ConvertCurrency();
+
+                EnableDisableStartPanel(false);
+            }
+            catch (Exception ex)
+            {
+                ResetStartPageToNormal($"Got Error of {ex.Message} while downloading currency values from the api!");
+            }
+        }
+        #endregion Data Parsing
+
+        #region HTTP Request
+        private async Task<string> SendHTTPRequest(string requestUri, Action<string> errorCallback)
+        {
+            try
+            {
+                using (HttpClient httpClient = new HttpClient())
+                {
+                    //Setting base uri of httpClient
+                    httpClient.BaseAddress = new Uri(baseUri);
+
+                    //Setting timeout time of httpClient to 10 seconds
+                    httpClient.Timeout = TimeSpan.FromSeconds(10);
+
+                    //Clearing any previous header adding a header to accept JSON format
+                    httpClient.DefaultRequestHeaders.Accept.Clear();
+                    httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+                    HttpResponseMessage httpResponse = await httpClient.GetAsync(requestUri);
+
+                    //Confirming that the http request must confirms that the request is successful or not
+                    httpResponse.EnsureSuccessStatusCode();
+
+                    //Reading the returned string data and returning
+                    return await httpResponse.Content.ReadAsStringAsync();
+                }
+            }
+            catch (HttpRequestException requestEx)
+            {
+                string errorMessage = "An error occurred during the API request:" + requestEx.Message;
+                //show error window
+                errorCallback?.Invoke(errorMessage);
+            }
+            catch (Exception ex)
+            {
+                string errorMessage = "An error occurred during the API request:" + ex.Message;
+                //show error window
+                errorCallback?.Invoke(errorMessage);
+            }
+
+            return string.Empty;
+        }
+
+        #endregion HTTP Request
+
     }
 }
