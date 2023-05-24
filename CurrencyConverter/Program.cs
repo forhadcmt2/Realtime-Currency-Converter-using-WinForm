e5ca5d6345e0f7cee0f050933b5564898dcc8319
Forhad Hossain <forhad@ghost.com.bd>
2023-05-24 13:02:07 +0600

    Unnecessary code removed

--- CurrencyConverter/Program.cs
+++ CurrencyConverter/Program.cs
@@ -19,29 +19,5 @@ namespace CurrencyConverter
             ApplicationConfiguration.Initialize();
             Application.Run(new Form1());
         }
-
-        public static void GetAllTheCurrencies()
-        {
-            HttpClient client = new HttpClient();
-            client.BaseAddress = new Uri("https://cdn.jsdelivr.net/");
-
-            // Add an Accept header for JSON format.
-            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
-
-            //Get Response from the api
-            HttpResponseMessage response = client.GetAsync("gh/fawazahmed0/currency-api@1/latest/currencies.json").Result;
-
-            if(response != null && response.IsSuccessStatusCode)
-            {
-
-                JsonNode? jObj = JsonObject.Parse(response.Content.ReadAsStringAsync().Result);
-
-                string currencies = response.Content.ReadAsStringAsync().Result;
-                using JsonDocument jsonDoc = JsonDocument.Parse(currencies);
-                //var jObj = JsonObject.Parse(currencies);
-                //var obj = jObj.AsObject();
-                //var value = obj.Select;
-            }
-        }
     }
 }
